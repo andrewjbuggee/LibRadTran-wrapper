cfbdf9205dbf1bfb009ae5c8afb451aaf9416c31
andrewbuggee <andrewbuggee@192.168.0.104>
2021-05-12 17:54:36 -0600

    Add runUVSPEC which will create the .inp file and run the uvspec program

---
+++ runUVSPEC.m
@@ -0,0 +1,12 @@
+%% --- Read Output Files from UVSPEC ---
+% ======================================
+% The purpose of this script is to run uvspec with the inputs specfied in
+% by the user. The user will be required to specify the atmospheric
+% composition, RTE solver, wavelength range, wavelength resolution, aerosol
+% content, cloud content, and solar spectrum. This file will then be fed
+% into the command line to run uvspec.
+
+
+clear all; close all;
+% --- By Andrew J. Buggee ---
+%% 
