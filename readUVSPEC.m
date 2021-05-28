496fca1b9337699f798df4b8ccd33c7e4591a47c
andrewbuggee <andrewbuggee@cu-biot-3-10.203.139.64.int.colorado.edu>
2021-05-28 17:13:47 -0600

    Had to change UVSPEC so it could account for two different rte solvers

--- readUVSPEC.m
+++ readUVSPEC.m
@@ -11,7 +11,7 @@
 % --- By Andrew J. Buggee ---
 %% --- Read in Files ---
 
-function [dataStruct,headers_units] = readUVSPEC(path,fileName)
+function [dataStruct,headers_units] = readUVSPEC(path,fileName,rte_solver)
 
 
 % [fileName,path] = uigetfile('.txt');
@@ -23,30 +23,75 @@ data = importdata([path,fileName],delimeter,headerLine);
 
 %% --- Converstion to a Structure ---
 
-% if the RTE solver is disort, sdisort, or spsdisort
+% if the RTE solver is disort, sdisort, or spsdisort, it has the following
+% outputs
+if strcmp(rte_solver,'disort')==true
+    
+    headers_units = cell(2,8);
+    headers_units{1,1} = 'wavelength';
+    headers_units{1,2} = 'dirIrrad';
+    headers_units{1,3} = 'difDwnIrrad';
+    headers_units{1,4} = 'difUpIrrad';
+    headers_units{1,5} = 'dirContMeanIrrad';
+    headers_units{1,6} = 'difDwnContMeanIrrad';
+    headers_units{1,7} = 'difUpContMeanIrrad';
+    headers_units{2,1} = 'nm';
+    headers_units{2,2} = 'mW/(m^{2} nm)';
+    headers_units{2,3} = 'mW/(m^{2} nm)';
+    headers_units{2,4} = 'mW/(m^{2} nm)';
+    headers_units{2,5} = 'mW/(m^{2} nm)';
+    headers_units{2,6} = 'mW/(m^{2} nm)';
+    headers_units{2,7} = 'mW/(m^{2} nm)';
+    
+    % Using these default outputs, we will want the following additional
+    % outputs
+    
+    data = [data,data(:,2)+data(:,3)];
+    headers_units{1,8} = 'totDwnIrrad';
+    headers_units{2,8} = 'mW/(m^{2} nm)';
+    
+    % and now we'll create the structure
+    
+    dataStruct = struct(headers_units{1,1},data(:,1),headers_units{1,2},data(:,2),...
+        headers_units{1,3},data(:,3),headers_units{1,4},data(:,4),...
+        headers_units{1,5},data(:,5),headers_units{1,6},data(:,6),...
+        headers_units{1,7},data(:,7),headers_units{1,8},data(:,8));
+    
+elseif strcmp(rte_solver,'twostr')==true
+    
+    headers_units = cell(2,6);
+    headers_units{1,1} = 'wavelength';
+    headers_units{1,2} = 'dirIrrad';
+    headers_units{1,3} = 'difDwnIrrad';
+    headers_units{1,4} = 'difUpIrrad';
+    headers_units{1,5} = 'dirContMeanIrrad';
+    headers_units{2,1} = 'nm';
+    headers_units{2,2} = 'mW/(m^{2} nm)';
+    headers_units{2,3} = 'mW/(m^{2} nm)';
+    headers_units{2,4} = 'mW/(m^{2} nm)';
+    headers_units{2,5} = 'mW/(m^{2} nm)';
+    
+    % Using these default outputs, we will want the following additional
+    % outputs
+    
+    data = [data,data(:,2)+data(:,3)];
+    headers_units{1,6} = 'totDwnIrrad';
+    headers_units{2,6} = 'mW/(m^{2} nm)';
+    
+    % and now we'll create the structure
+    
+    dataStruct = struct(headers_units{1,1},data(:,1),headers_units{1,2},data(:,2),...
+        headers_units{1,3},data(:,3),headers_units{1,4},data(:,4),...
+        headers_units{1,5},data(:,5),headers_units{1,6},data(:,6));
+    
+    
+    
+else
+    error('Dont recognize rte solver')
+    
+    
+end
 
-headers_units = cell(2,7);
-headers_units{1,1} = 'wavelength';
-headers_units{1,2} = 'dirIrrad';
-headers_units{1,3} = 'difDwnIrrad';
-headers_units{1,4} = 'difUpIrrad';
-headers_units{1,5} = 'dirContMeanIrrad';
-headers_units{1,6} = 'difDwnContMeanIrrad';
-headers_units{1,7} = 'difUpContMeanIrrad';
-headers_units{2,1} = 'nm';
-headers_units{2,2} = 'mW/(m^{2} nm)';
-headers_units{2,3} = 'mW/(m^{2} nm)';
-headers_units{2,4} = 'mW/(m^{2} nm)';
-headers_units{2,5} = 'mW/(m^{2} nm)';
-headers_units{2,6} = 'mW/(m^{2} nm)';
-headers_units{2,7} = 'mW/(m^{2} nm)';
-
-% and now we'll create the structure
-
-dataStruct = struct(headers_units{1,1},data(:,1),headers_units{1,2},data(:,2),...
-    headers_units{1,3},data(:,3),headers_units{1,4},data(:,4),...
-    headers_units{1,5},data(:,5),headers_units{1,6},data(:,6),...
-    headers_units{1,7},data(:,7));
 
 
 
