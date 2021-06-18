2d52f95b6e7c5e00e1675482fdff7293757ad994
andrewbuggee <andrewbuggee@192.168.0.104>
2021-06-18 11:58:20 -0600

    output the source file along with input settings

--- runUVSPEC.m
+++ runUVSPEC.m
@@ -41,6 +41,8 @@ expr3 = '[^\n]*phi [^\n]*';
 expr4 = '[^\n]*sza [^\n]*';
 expr5 = '[^\n]*phi0 [^\n]*';
 expr6 = '[^\n]*zout [^\n]*';
+expr7 = '[^\n]*source [^\n]*';
+expr8 = '[^\n]*wavelength [^\n]*';
 
 match1 = regexp(textFile,expr1,'match'); % find rte_solver typ
 match2 = regexp(textFile,expr2,'match'); % find consine of viewing angle vector
@@ -48,7 +50,8 @@ match3 = regexp(textFile,expr3,'match'); % find azimuth viewing angle vector
 match4 = regexp(textFile,expr4,'match'); % find the solar zenith angle
 match5 = regexp(textFile,expr5,'match'); % find the solar azimuth angle
 match6 = regexp(textFile,expr6,'match'); % find the sensor altitude
-
+match7 = regexp(textFile,expr7,'match'); % find the source file
+match8 = regexp(textFile,expr8,'match'); % find the wavelength range in order to trim the source file
 
 index1 = find(match1{1}==' '); % find the spaces
 
@@ -63,6 +66,12 @@ index4_spaces = regexp(match4{1},'\s'); % find the spaces. There is only 1 value
 index5_spaces = regexp(match5{1},'\s'); % find the spaces. There is only 1 value per file for the solar azimuth
 
 index6_spaces = regexp(match6{1},'\s'); % find the spaces. There is only 1 value per file for the solar azimuth
+
+index7_spaces = regexp(match7{1},'\s'); % find the spaces
+index7_slashes = regexp(match7{1},'[/]'); % Brackets treat the symbol literally. number of decimals tells us how many values there are in the vector
+
+index8_spaces = regexp(match8{1},'\s'); % find the spaces
+index8_dots = regexp(match8{1},'[.]'); % Brackets treat the symbol literally. number of decimals tells us how many values there are in the vector
 
 
 
@@ -103,7 +112,36 @@ else
    zout = str2double(zout);
 end
 
+% find the wavelength range of the output file
+wavelength_str= cell(1,length(index8_dots));
 
+for ii = 1:length(index8_dots)
+    wavelength_str{ii} = match8{1}(index8_spaces(ii)+1:index8_spaces(ii+1)-1);
+end
+wavelength = str2double(wavelength_str);
+
+% determine the solar or thermal source file
+% all solar source files will be located in the folder: /Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval-Research/LibRadTran/libRadtran-2.0.4/data/solar_flux
+% all thermal source files will be located in the foler: 
+if strcmp('solar',match7{1}(index7_spaces(1)+1:index7_spaces(2)-1))
+    folderSolar = ['/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval-Research/',...
+                    'LibRadTran/libRadtran-2.0.4/data/solar_flux/'];
+    fileSolar = match7{1}(index7_slashes(end)+1:index7_spaces(3)-1);
+    sourceFile = fileread([folderSolar,fileSolar]);
+    
+    exprSource = '[^\n]*[\d][\d] [^\n]*'; % look for the new lines with atleast two digits in a row
+    matchSource = regexp(sourceFile,exprSource,'match'); % find rte_solver typ
+    
+    source = zeros(length(matchSource),2);
+    for ii = 1:length(matchSource)
+        source(ii,:) = str2num(matchSource{ii});
+    end
+    
+    % now we clip source to match the length of our wavelength vector
+    indexSource = source(:,1)>=wavelength(1) & source(:,1)<=wavelength(2);
+    source = source(repmat(indexSource,1,2));
+    source = reshape(source,size(source,1)/2,[]);
+end
 
 % Pull all input settings into a cell array
 inputSettings{1} = rte_solver;
@@ -112,6 +150,7 @@ inputSettings{3} = phiVec;
 inputSettings{4} = sza;
 inputSettings{5} = saz;
 inputSettings{6} = zout;
+inputSettings{7} = source;
 
 %% Running the .INP file from the command line
 
