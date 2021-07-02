13568e3777a8896c67ced613c71aa754584abd92
andrewbuggee <andrewbuggee@192.168.0.104>
2021-07-01 18:27:15 -0600

    Lots of beautiful efficient changes!

--- edit_INP_DAT_filesWrapper.m
+++ edit_INP_DAT_filesWrapper.m
@@ -5,17 +5,18 @@
 % By Andrew J. Buggee
 %% --- EDIT .INP Files ---
 
+wavelengthBand = '2_14';
+
 oldFolder = ['/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval-Research/',...
-            'LibRadTran/libRadtran-2.0.4/Reflectance_Function_MODIS_3_72mu/'];
+            'LibRadTran/libRadtran-2.0.4/Reflectance_Function_MODIS_',wavelengthBand,'mu_rev2/'];
         
-newFolder = ['/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval-Research/',...
-            'LibRadTran/libRadtran-2.0.4/Reflectance_Function_MODIS_3_72mu_rev2/'];
+newFolder = oldFolder;
 
 
 
-oldExpr = 'umu 0.5';
+oldExpr = 'umu 0.0';
 
-newExpr = 'umu 0.0';
+newExpr = 'umu 1.0';
 
 
 % introduce change variables in the file name
@@ -31,7 +32,7 @@ for ii = 1:length(re)
     for jj = 1:length(tau_c)
         
         % redefine the old file each time
-        oldFile = ['3_72_micronBand_droplets_',num2str(re(ii)),'_microns_tau_',num2str(tau_c(jj)),'.INP'];
+        oldFile = [wavelengthBand,'_micronBand_droplets_',num2str(re(ii)),'_microns_tau_',num2str(tau_c(jj)),'.INP'];
         newFile = oldFile;
         
         edit_INP_DAT_files(oldFolder,newFolder,oldFile,newFile,oldExpr,newExpr);
