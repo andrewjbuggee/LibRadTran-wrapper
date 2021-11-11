53f4ad99f746354b26e5f63365be0ad9d1332547
anbu8374 <anbu8374@macd3715.lasp.colorado.edu>
2021-11-11 10:41:05 -0700

    Changes to run mutliple pixels in parallel

---
+++ addLibRadTran_paths.m
@@ -0,0 +1,13 @@
+% --- Add Folders to Path specific to this computer -----
+
+% LibRadTran Data Folders
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/solar_flux/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/wc/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/albedo/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/altitude/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/atmmod/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/correlated_k/');
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/wc/');
+
+% LibRadTran Bin folder
+addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/bin/');
