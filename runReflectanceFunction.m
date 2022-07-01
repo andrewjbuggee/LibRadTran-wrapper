b00ae48580acc8e2311bbab7bbf5987ba49e7f95
anbu8374 <anbu8374@macd3715.lasp.colorado.edu>
2022-07-01 17:29:41 -0600

    some changes were made

--- runReflectanceFunction.m
+++ runReflectanceFunction.m
@@ -15,14 +15,14 @@ function [R,Rl] = runReflectanceFunction(inputs,names)
 userName = whatComputer;
 
 if strcmp(userName,'anbu8374')
-    
+
     libRadTran_path = ['/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4'];
-    
+
 elseif strcmp(userName,'andrewbuggee')
-    
+
     libRadTran_path = ['/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/',...
         'LibRadTran/libRadtran-2.0.4'];
-    
+
 else
     error('I dont recognize this computer user name')
 end
@@ -32,10 +32,10 @@ addpath(libRadTran_path);
 
 % ----- extract inputs -----
 
-re = inputs.re;
-tau_c = inputs.tau_c;
+
 inp_folder = [libRadTran_path,'/',inputs.INP_folderName]; % where the newly created .inp files will be saved
 saveCalcs_filename = inputs.saveCalculations_fileName;
+saveCalcs_folder = inputs.savedCalculations_folderName;
 inputFileNames = names.inp;
 outputFileNames = names.out;
 
@@ -48,78 +48,75 @@ Rl = cell(size(inputFileNames)); % each value here is the spectral reflectance o
 
 % first step through pixel space
 for pp = 1:size(inputFileNames,1)
-    
+
     % next step through the band dimension
     for bb = 1:size(inputFileNames,4)
-        
-        
+
+
         % next step through the different values for r per band
         parfor rr = 1:size(inputFileNames,2)
-            
-            
+
+
             % if pp==1 && bb ==1 && rr == 1
-                
-                % there is a new geometry setting every time we switch
-                % pixels. So we need a new input settings for when we
-                % switch pixels
 
-                % --- For now, calculate inputSettings every time ---
-                
-                % start by running uvspec
-                [inputSettings] = runUVSPEC(inp_folder,inputFileNames(pp,rr,:,bb),outputFileNames(pp,rr,:,bb));
-                
-                % save the input settings file. In this case each band will have the
-                % same geometry so we only need to save a single settings file per band
-%                 save([inp_folder,'runUVSPEC_settings.mat'],"inputSettings"); % save inputSettings to the same folder as the input and output file
-                
-%             else
-%                 runUVSPEC(inp_folder,inputFileNames(pp,rr,:,bb),outputFileNames(pp,rr,:,bb));
-%             end
-            
+            % there is a new geometry setting every time we switch
+            % pixels. So we need a new input settings for when we
+            % switch pixels
 
-      
+            % --- For now, calculate inputSettings every time ---
+
+            % start by running uvspec
+            [inputSettings] = runUVSPEC(inp_folder,inputFileNames(pp,rr,:,bb),outputFileNames(pp,rr,:,bb));
+
+            % save the input settings file. In this case each band will have the
+            % same geometry so we only need to save a single settings file per band
+            %                 save([inp_folder,'runUVSPEC_settings.mat'],"inputSettings"); % save inputSettings to the same folder as the input and output file
+
+            %             else
+            %                 runUVSPEC(inp_folder,inputFileNames(pp,rr,:,bb),outputFileNames(pp,rr,:,bb));
+            %             end
+
+
+
             % read in the data structure and calculate reflectance function
-            
+
             ds = cell(1,length(outputFileNames(pp,rr,:,bb)));
-            
+
             % --- IMPORTANT ----
             % for the for loop to run, we must define the range of the
             % forloop using a fixed value outside of the parfor loop. The
             % range cannot be defined by calling a sliced variable
-            
+
             % Finally, step through the different values of optical thickness
             for tt = 1:length_tau
-                
+
                 [pp,rr,tt,bb]
 
                 [ds{tt},~,~] = readUVSPEC(inp_folder,outputFileNames{pp,rr,tt,bb},inputSettings(tt+1,:)); % headers don't change per iteration
                 [R(pp,rr,tt,bb),Rl{pp,rr,tt,bb}] = reflectanceFunction(inputSettings(tt+1,:),ds{tt});
-                
+
             end
-            
-            
+
+
         end
-        
-        
-        
+
+
+
     end
-    
+
 end
 
 
-% Check to make sure you're in the modis data directory
 
-if strcmp(pwd, inputs.modisDataFolder)==true
+% Change to the proper MODIS data folder
+cd(saveCalcs_folder)
+
+% Check to see that the file exists
+if isfile(saveCalcs_filename)==true
     % save the relfectance calculation
     save(saveCalcs_filename,"R",'-append'); % save inputSettings to the same folder as the input and output file
-        
 else
-    
-    % Change to the proper MODIS data folder
-    cd(inputs.modisDataFolder)
-    % save the relfectance calculation
-    save(saveCalcs_filename,"R",'-append'); % save inputSettings to the same folder as the input and output file
-
+    save(saveCalcs_filename, "inputs", "R");
 end
 
 
--- write_wc_file.m
+++ write_wc_file.m
@@ -69,6 +69,17 @@
 %       chosen, the code expects a single column vector for re, or a
 %       matrix, where each column vector represents a cloud
 
+
+%   (7) parameterization_str - a string telling the code which
+%   parameterization to use when using optical depth and droplet radius to
+%   compute the liquid water content. There are 2 options
+%       (a) 'mie' - this option uses a pre-computed mie table and
+%       interpolates to find values for the extinction efficiency at each
+%       wavelength and each droplet radius
+%       (b) '2Limit' - this option assumes we are close to the extinction
+%       paradox limit and sets the extinction efficiency at a constant
+%       value of 2
+
 % OUTPUTS:
 %   (1) .Dat file saved in the libRadTran folder:
 %   /.../libRadtran-2.0.4/data/wc
@@ -80,7 +91,7 @@
 
 %%
 
-function [fileName] = write_wc_file(re,tau_c,z_topBottom, lambda, distribution_str, homogeneous_str)
+function [fileName] = write_wc_file(re,tau_c,z_topBottom, lambda, distribution_str, homogeneous_str, parameterization_str)
 
 % ------------------------------------------------------------
 % ---------------------- CHECK INPUTS ------------------------
@@ -90,9 +101,9 @@ function [fileName] = write_wc_file(re,tau_c,z_topBottom, lambda, distribution_s
 % depth, and the altitude vector associated with this cloud
 
 
-if nargin~=6
+if nargin~=7
     error([newline,'Not enough inputs. Need 6: droplet effective radius, optical depth, altitude',...
-        ' wavelength, droplet distribution type and the homogeneity type.', newline])
+        ' wavelength, droplet distribution type, homogeneity type and the parameterization used to compute LWC.', newline])
 end
 
 % Check to make sure re is the same length as the altitude vector
@@ -103,35 +114,35 @@ if size(z_topBottom,1)==1 || size(z_topBottom,2)==1
     if length(z_topBottom)~=2
         error([newline,'Need two values for z_topBottom: altitude at cloud bottom top and cloud bottom', newline])
     end
-    
+
     % make sure its a  column vector
     z_topBottom = reshape(z_topBottom,[],1);
     H = z_topBottom(1) - z_topBottom(2);            % geometric thickness
-    
+
 elseif size(z_topBottom,1)>1 && size(z_topBottom,2)>1
     % if true, then there can only be two rows, and it must be equal in
     % size to the r matrix
     if size(z_topBottom,1)~=2
         error([newline,'Need two values for z_topBottom: altitude at cloud bottom top and cloud bottom', newline])
-        
+
     elseif size(z_topBottom,2)~=size(re,2) || size(z_topBottom,2)==1
         error([newline,'z_topBottom must have the same number of columns as re, or a single column that is used for all wc files.', newline])
-        
+
     end
-    
+
     H = z_topBottom(:,1) - z_topBottom(:,2);        % geometric cloud thickness
-    
+
 end
 
 
 
 if length(lambda)>1 && length(lambda)~=size(re,2)
-    
+
     error([newline,'Lambda must be either a single value or a vector equal in legnth to the number of columns in re.', newline])
 end
 
 if length(tau_c)>1 && length(tau_c)~=size(re,2)
-    
+
     error([newline,'The toptical depth must be either a single value or a vector equal in legnth to the number of columns in re.', newline])
 end
 
@@ -160,7 +171,7 @@ r_eff_bounds = [1, 100];                    % microns - effective radius boundar
 % Check to make sure the distribution string is one of two possible values
 
 if strcmp(distribution_str, 'mono')==false && strcmp(distribution_str, 'gamma')==false
-    
+
     error([newline,'I dont recognize the droplet distribution. Must be either "mono" or "gamma"', newline])
 end
 
@@ -168,7 +179,7 @@ end
 % Check to make sure the homogeneity string is one of two possible values
 
 if strcmp(homogeneous_str, 'homogeneous')==false && strcmp(homogeneous_str, 'non-homogeneous')==false
-    
+
     error([newline,'I dont recognize the homogeneity string. Must be either "homogenous" or "non-homogeneous"', newline])
 end
 
@@ -185,29 +196,29 @@ if size(re,1)>1 && size(re,2)>1
             'This is the default parameterization used to convert water cloud parameters to optical properites.',newline,...
             'The range of acceptable values for this parameterization is [2.5, 60] microns.',newline]);
     end
-    
+
     if any(any(re>25))
         warning([newline,'At least one value in r_{e} is greater than 25 microns, which is the upper limit',...
             ' to the Mie Interpolate parameterization that computes optical properties from the water cloud ',...
             'parameters. The netcdf file downloaded from LibRadTrans website includes values of re up to',...
             ' 25 microns. The acceptable range for Mie Interpolation is [1, 25] micorns.',newline]);
     end
-    
+
 else
-    
+
     if any(re<2.5) || any(re>60)
         warning([newline, 'At least one value in r_{e} is outside the range of the Hu and Stamnes parameterization',newline,...
             'This is the default parameterization used to convert water cloud parameters to optical properites.',newline,...
             'The range of acceptable values for this parameterization is [2.5, 60] microns.',newline]);
     end
-    
+
     if any(re>25)
         warning([newline,'At least one value in r_{e} is greater than 25 microns, which is the upper limit',...
             ' to the Mie Interpolate parameterization that computes optical properties from the water cloud ',...
             'parameters. The netcdf file downloaded from LibRadTrans website includes values of re up to',...
             ' 25 microns. The acceptable range for Mie Interpolation is [1, 25] micorns.',newline]);
     end
-    
+
 end
 
 
@@ -219,15 +230,15 @@ computer_name = whatComputer;
 % Find the folder where the mie calculations are stored
 % find the folder where the water cloud files are stored.
 if strcmp(computer_name,'anbu8374')==true
-    
+
     mie_calc_folder_path = '/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/Mie_Calculations/';
     water_cloud_folder_path = '/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/wc/';
-    
+
 elseif strcmp(computer_name,'andrewbuggee')==true
-    
+
     mie_calc_folder_path = '/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/Mie_Calculations/';
     water_cloud_folder_path = '/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/wc/';
-    
+
 end
 
 %%
@@ -236,99 +247,127 @@ end
 % ---------------------- COMPUTE LWC -------------------------
 % ------------------------------------------------------------
 
+% -------------------------------------------------
 % ----- I have to fudge the density of water ------
 % This is the only way I can get my estimates of optical depth to match
 % LibRadTrans estimates
 
-%rho_liquid_water = 859900;              % grams/m^3 - density of liquid water
-rho_liquid_water = 1e6;                 % grams/cm^3 - density of liquid water at 0 C
-
+rho_liquid_water = 959900;              % grams/m^3 - density of liquid water
+%rho_liquid_water = 1e6;                 % grams/cm^3 - density of liquid water at 0 C
+% --------------------------------------------------
 
 
 % --- STEP THROUGH COLUMNS OF re -----
 
 if size(re,1)>1 && size(re,2)>1
-    
+
     % We want to get all the mie properties we need before we loop through
     % the number of files
-    
+
     num_files_2write = size(re,2);          % number of wc files to create
-    
-    
-    
+
+
+
     % -------------------------------------------------------------------
     % ------ open the precomputed mie table and interpolate! ------------
     % -------------------------------------------------------------------
-    
+
     % for writing water cloud files, we only need the extinction efficiency
     % Since this function is used often, we've created a file with just Q_ext
-    
+
     justQ = true;                       % Load the precomputed mie table of only Q_ext values
-    
-    % lambda has to be the same size as re
-    if length(lambda)==num_files_2write
-        
-        % remember that vectorizing a matrix (:) will stack column vectors
-        % on top of one another
-        
-        lambda = reshape(lambda,1,[]); % each column represents a new file
-        
-        % create identical column vecotrs
-        lambda = repmat(lambda,size(re,1),1);
-        
-        yq = interp_mie_computed_tables([lambda(:), re(:)], distribution_str, justQ);
-        
-    elseif length(lambda)==1
-        
-        yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re(:)], distribution_str, justQ);
-        
+
+
+
+    if strcmp(parameterization_str, 'mie')==true
+
+        % lambda has to be the same size as re
+        if length(lambda)==num_files_2write
+
+            % remember that vectorizing a matrix (:) will stack column vectors
+            % on top of one another
+
+            lambda = reshape(lambda,1,[]); % each column represents a new file
+
+            % create identical column vecotrs
+            lambda = repmat(lambda,size(re,1),1);
+
+            yq = interp_mie_computed_tables([lambda(:), re(:)], distribution_str, justQ);
+
+
+
+        elseif length(lambda)==1
+
+            yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re(:)], distribution_str, justQ);
+
+        end
+
+    elseif strcmp(parameterization_str,'2limit')==true
+        % set the value to be 2 for all calculations
+        yq = 2*ones(length(re),5);
+
     end
-    
-    
-    
-    
+
+
+
+
+
 elseif (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'non-homogeneous')==true
-    
+
     num_files_2write = 1;
-    
+
     % re must be a column vector
     re = reshape(re,[],1);
-    
+
     % -------------------------------------------------------------------
     % ------ open the precomputed mie table and interpolate! ------------
     % -------------------------------------------------------------------
-    
+
     % for writing water cloud files, we only need the extinction efficiency
     % Since this function is used often, we've created a file with just Q_ext
-    
+
     justQ = true;                       % Load the precomputed mie table of only Q_ext values
-    
-    yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re], distribution_str, justQ);
-    
-    
+
+    if strcmp(parameterization_str,'mie')==true
+
+        yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re], distribution_str, justQ);
+
+    elseif strcmp(parameterization_str, '2limit')==true
+
+        yq = 2*ones(length(re),5);
+
+    end
+
+
 elseif (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'homogeneous')==true
-    
+
     num_files_2write = length(re);
-    
+
     % re must be a column vector
     re = reshape(re,[],1);
-    
+
     % -------------------------------------------------------------------
     % ------ open the precomputed mie table and interpolate! ------------
     % -------------------------------------------------------------------
-    
+
     % for writing water cloud files, we only need the extinction efficiency
     % Since this function is used often, we've created a file with just Q_ext
-    
+
     justQ = true;                       % Load the precomputed mie table of only Q_ext values
-    
-    yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re], distribution_str, justQ);
-    
-    
+
+    if strcmp(parameterization_str,'mie')==true
+        yq = interp_mie_computed_tables([repmat(lambda,numel(re),1), re], distribution_str, justQ);
+
+    elseif strcmp(parameterization_str, '2limit')==true
+        yq = 2*ones(length(re),5);
+
+    end
+
+
 else
-    
+
     error([newline,'re is not a vector or a matrix! Check your inputs!', newline])
-    
+
 end
 
 % grab the q extinction values
@@ -337,7 +376,7 @@ if justQ==false
     Qext = reshape(yq(:,5),[],num_files_2write);         % Extinction efficiency
 else
     Qext = reshape(yq(:,3),[],num_files_2write);         % conver this back into a matrix corresponging to re
-    
+
 end
 
 
@@ -371,156 +410,156 @@ elseif (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'non-homogene
     nLayers = length(re)+1;             % Number of altitude levels we need to define a cloud
 elseif (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'homogeneous')==true
     nLayers = 1;
-    
+
 end
 
-    
+
 for nn = 1:num_files_2write
-    
-    
+
+
     % -------------------------------------------
     % ------ Create altitude vector! ------------
     % -------------------------------------------
-    
+
     % the length of the altitude vector should be 1 unit longer than the length
     % of the effective radius. Thats because the last value in the altitude
     % vector is the altitude at cloud top, where the LWC has gone to zero
-    
-    
+
+
     % z must be a column vector
     if nLayers==1
         z = flipud(z_topBottom(:,nn));
-        
+
     else
         z = linspace(z_topBottom(2,nn), z_topBottom(1,nn), nLayers)';                 % km - altitude vector
     end
-    
-    
+
+
     % -------------------------------------------------------------------
     % ------------------- compute number concentration ------------------
     % -------------------------------------------------------------------
-    
+
     % we need a number concentration for each file that is created
-    
+
     if nLayers>1
         Nc = tau_c(nn)./(pi*trapz((z(1:end-1)-z(1))*1e3,Qext(:,nn).*(re(:,nn)*1e-6).^2));                % m^(-3) - number concentration
-        
+
         % Compute Liquid Water Content
         lwc = 4/3 * pi * rho_liquid_water * (re(:,nn)*1e-6).^3 .* Nc;                    % g/m^3 - grams of water per meter cubed of air
-        
+
         % create the water cloud file name
         fileName{nn} = ['WC_rtop',num2str(round(re(end,nn))),'_rbot',num2str(round(re(1,nn))),'_T',num2str(round(tau_c(nn))),'_', distribution_str, '.DAT'];
-        
+
     else
         Nc = tau_c(nn)./(pi*(H(nn)*1e3)*Qext(nn).*(re(nn)*1e-6).^2);                            % m^(-3) - number concentration
-        
+
         % Compute Liquid Water Content
         lwc = 4/3 * pi * rho_liquid_water * (re(nn)*1e-6).^3 .* Nc;                    % g/m^3 - grams of water per meter cubed of air
-        
+
         % create the water cloud file name
         fileName{nn} = ['WC_r',num2str(round(re(nn))),'_T',num2str(round(tau_c(nn))),'_', distribution_str, '.DAT'];
-        
+
     end
-    
-    
-    
+
+
+
     % ------------------------------------------------------------
     % ----------------- WE NEED TO APPEND ZEROS ------------------
     % ------------------------------------------------------------
-    
+
     % Wherever the cloud is, there needs to be zeros at the cloud top altitude,
     % and below the cloud bottom altitude. This information tells LibRadTran
     % where the boundaries of the cloud are
-    
+
     % both the effective radius and the LWC need zeros on either boundary,
     % unless if the cloud is at the surface
-    
+
     if (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'non-homogeneous')==true
-        
+
         if z_topBottom(2)==0
             % If true, then the cloud starts at the surface and we only append
             % zeros above the cloud
             re_2write = [re; 0];
             lwc_2write = [lwc; 0];
             z_2write = z;
-            
+
         else
             % In this case, we need zeros below the cloud bottom, and at cloud
             % top
             z_2write = [0; z];                 % create a value at the surface where the cloud parameters go to zero
             re_2write = [0; re; 0];
             lwc_2write = [0; lwc; 0];
-            
+
         end
-        
+
     elseif (size(re,1)==1 || size(re,2)==1) && strcmp(homogeneous_str, 'homogeneous')==true
-        
+
         if z_topBottom(2)==0
             % If true, then the cloud starts at the surface and we only append
             % zeros above the cloud
             re_2write = [re(nn); 0];
             lwc_2write = [lwc; 0];
             z_2write = z;
-            
+
         else
             % In this case, we need zeros below the cloud bottom, and at cloud
             % top
             z_2write = [0; z];                 % create a value at the surface where the cloud parameters go to zero
             re_2write = [0; re(nn); 0];
             lwc_2write = [0; lwc; 0];
-            
+
         end
-        
+
     elseif (size(re,1)>1 && size(re,2)>1)
-        
+
         % Cloud top height defines the altitude where there is no cloud.
-        
+
         % if the minimum z value is 0 then the cloud is at the surface
         if z_topBottom(2)==0
             % then we only append zeros above the cloud
             re_2write = [re(:,nn); 0];
             lwc_2write = [lwc; 0];
             z_2write = z;
-            
+
         else
             % Then we need zeros on either end
             z_2write = [0; z];
             re_2write = [0; re(:,nn); 0];
             lwc_2write = [0; lwc; 0];
-            
+
         end
-        
+
     end
-    
-    
+
+
     % ------------------------------------------------------------
     % ---------------------- WRITE WC FILE -----------------------
     % ------------------------------------------------------------
-    
-    
-    
+
+
+
     % Create the water cloud file
     fileID = fopen([water_cloud_folder_path,fileName{nn}], 'w');
-    
+
     % fprintf writes lines in our text file from top to botom
     % wc.DAT files are written with the higher altitudes at the top, and the
     % surface at the bottom
-    
+
     % to write column vectors in a text file, we have to store them as row
     % vectors
-    
+
     toWrite = [flipud(z_2write)'; flipud(lwc_2write)'; flipud(re_2write)'];
-    
+
     % Create the opening comment lines of the WC.DAT file
-    
+
     fprintf(fileID, '%s %10s %7s %8s \n','#','z','LWC','R_eff');
     fprintf(fileID, '%s %10s %7s %8s \n','#','(km)','(g/m^3)','(micron)');
-    
+
     % Write in the data
     fprintf(fileID,'%12.3f %7.4f %8.3f \n', toWrite);
     fclose(fileID);
-    
-    
+
+
 end
 
 
