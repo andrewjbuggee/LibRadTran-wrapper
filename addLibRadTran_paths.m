c434d7be0310b1940fa4d9f1cc461a02a828164c
anbu8374 <anbu8374@macd3715.lasp.colorado.edu>
2023-11-16 15:30:35 -0700

    super computer paths

--- addLibRadTran_paths.m
+++ addLibRadTran_paths.m
@@ -3,29 +3,44 @@
 computer_name = whatComputer;
 
 if strcmp(computer_name,'anbu8374')==true
-% LibRadTran Data Folders
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/solar_flux/');
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/wc/');
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/albedo/');
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/altitude/');
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/atmmod/');
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/correlated_k/');
+    % ----- LASP MAC ------
+    % LibRadTran Data Folders
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/solar_flux/');
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/wc/');
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/albedo/');
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/altitude/');
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/atmmod/');
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/data/correlated_k/');
 
-% LibRadTran Bin folder
-addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/bin/');
+    % LibRadTran Bin folder
+    addpath('/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4/bin/');
 
 elseif strcmp(computer_name,'andrewbuggee')==true
-    
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/solar_flux/');
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/wc/');
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/albedo/');
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/altitude/');
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/atmmod/');
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/correlated_k/');
+    % ----- MACBOOK ------
 
-% LibRadTran Bin folder
-addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/bin/');
+
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/solar_flux/');
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/wc/');
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/albedo/');
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/altitude/');
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/atmmod/');
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/data/correlated_k/');
+
+    % LibRadTran Bin folder
+    addpath('/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/LibRadTran/libRadtran-2.0.4/bin/');
+
+
+elseif strcmp(computer_name,'curc')==true
+    % ----- CU SUPERCOMPUTER ------
+
+    % Add paths to all functions needed
+    addpath('/projects/anbu8374/Hyperspectral_Cloud_Retrieval/')
+    addpath('/projects/anbu8374/Hyperspectral_Cloud_Retrieval/Bayes_Inverse_Functions/')
+    addpath('/projects/anbu8374/LibRadTran-wrapper/')
+    addpath('/projects/anbu8374/MODIS-Cloud-Retrieval/')
+    addpath('/projects/anbu8374/MODIS_data/')
+    addpath('/projects/anbu8374/VOCALS_REx/')
+    addpath('/projects/anbu8374/Radiative_Transfer_Physics/')
 
 end
 
-    
